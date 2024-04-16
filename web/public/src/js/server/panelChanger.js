7a1bf94a3efe8e3f1216bfea5931f5dc85fc1c1c
Cauvin Pierre <73099815+sajima90@users.noreply.github.com>
2024-04-16 18:20:43 +0200

    + rechange

--- web/public/src/js/server/panelChanger.js
+++ web/public/src/js/server/panelChanger.js
@@ -1,3 +1,4 @@
+
 async function changerPanel(panel) {
     try {
         let result = await fetch('/' + panel);
@@ -6,20 +7,6 @@ async function changerPanel(panel) {
         } else {
             let text = await result.text();
             $("body").html(text);
-            // Obtenez toutes les balises script dans le nouveau contenu HTML
-            let parser = new DOMParser();
-            let doc = parser.parseFromString(text, "text/html");
-            let scripts = doc.getElementsByTagName('script');
-            // Ajoutez un paramètre de requête unique à l'URL de chaque script
-            for (let script of scripts) {
-                if (script.src) {
-                    let url = new URL(script.src);
-                    url.searchParams.set('t', Date.now());
-                    script.src = url.href;
-                }
-            }
-            // Remplacez le contenu du corps avec le nouveau contenu HTML
-            $("body").html(doc.body.innerHTML);
             console.log("Changement de panel -> " + panel);
         }
     } catch (e) {
